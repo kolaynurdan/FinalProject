36d4091900b27d59cf3544ec4bfb6642d856edd8
kolaynurdan <kolaynurdan@gmail.com>
2021-03-26 17:46:57 +0300

    updated Result

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,9 @@ namespace Business.Abstract
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min, decimal max);
         List<ProductDetailDto> GetProductDetails();
+
+        Product GetById(int productId);
+
+        IResult Add(Product product);
     }
 }
