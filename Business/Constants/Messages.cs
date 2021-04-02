a17a5f11dc45de1c0870eada777519a43482065d
kolaynurdan <kolaynurdan@gmail.com>
2021-04-02 23:33:08 +0300

    updated Business

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +9,7 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün eklendi.";
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
+        internal static string MaintenanceTime = "Sistem Bakimda";
+        internal static string ProductsListed;
     }
 }
