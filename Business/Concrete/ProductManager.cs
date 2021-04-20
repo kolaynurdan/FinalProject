dd84d4d53772acbec666f47f0ea8303adbbf0f0e
kolaynurdan <kolaynurdan@gmail.com>
2021-04-21 02:05:33 +0300

    Add project files.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,122 +1,111 @@
 ﻿using Business.Abstract;
-using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
-using Core.CrossCuttingConcerns.Validation;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using Entities.DTOs;
-using FluentValidation;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
+using Business.BusinessAspects.Autofac;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _productDal;
-        ICategoryService _categoryService;
+        private IProductDal _productDal;
+        private ICategoryService _categoryService;
 
         public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _productDal = productDal;
             _categoryService = categoryService;
-            
         }
 
-        [SecuredOperation("admin,editor")]
-        [ValidationAspect(typeof(ProductValidator))]
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        [PerformanceAspect(5)]
+        public IDataResult<List<Product>> GetList()
+        {
+            Thread.Sleep(5000);
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        [SecuredOperation("Product.List,Admin")]
+        [LogAspect(typeof(FileLogger))]
+        [CacheAspect(duration: 10)]
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+
+
+        [ValidationAspect(typeof(ProductValidator), Priority = 1)]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
-
-            //ValidationTool.Validate(new ProductValidator(), product);
-            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName), CheckIfProductCountOfCategoryCorrect(product.CategoryId), CheckIfCategoryLimitExceded());
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName), CheckIfCategoryIsEnabled());
 
             if (result != null)
             {
                 return result;
             }
-
             _productDal.Add(product);
-
             return new SuccessResult(Messages.ProductAdded);
-
-
-        }
-
-        public IDataResult<List<Product>> GetAll()
-        {
-            if (DateTime.Now.Hour == 1)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
-        }
-
-        public IDataResult<List<Product>> GetAllByCategoryId(int id)
-        {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
-        }
-
-        public IDataResult<Product> GetById(int productId)
-        {
-            return new SuccessDataResult<Product>(_productDal.Get(p=>p.ProductId == productId));
-        }
-
-        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
-        {
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >=min && p.UnitPrice<=max));
-        }
-
-        public IDataResult<List<ProductDetailDto>> GetProductDetails()
-        {
-            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
-        }
-
-        public IResult Update(Product product)
-        {
-            var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
-            if (result >= 10)
-            {
-                return new ErrorResult(Messages.ProductCountOfCategoryError);
-            }
-            throw new NotImplementedException();
-        }
-
-        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
-        {
-            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
-            if (result >= 15)
-            {
-                return new ErrorResult(Messages.ProductCountOfCategoryError);
-            }
-            return new SuccessResult();
         }
 
         private IResult CheckIfProductNameExists(string productName)
         {
-            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+
+            var result = _productDal.GetList(p => p.ProductName == productName).Any();
             if (result)
             {
                 return new ErrorResult(Messages.ProductNameAlreadyExists);
             }
+
             return new SuccessResult();
         }
 
-        private IResult CheckIfCategoryLimitExceded()
+        private IResult CheckIfCategoryIsEnabled()
         {
-            var result = _categoryService.GetAll();
-
-            if (result.Data.Count>15)
+            var result = _categoryService.GetList();
+            if (result.Data.Count < 10)
             {
-                return new ErrorResult(Messages.CategoryLimitExceded);
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
             }
+
             return new SuccessResult();
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        public IResult Update(Product product)
+        {
+
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+
+        [TransactionScopeAspect]
+        public IResult TransactionalOperation(Product product)
+        {
+            _productDal.Update(product);
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
